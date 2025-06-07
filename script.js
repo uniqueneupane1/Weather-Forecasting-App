d04052cd8f866ad75170adec7fe88548f5aa8237
Unique Neupane <uniqueneupane935@gmail.com>
2025-06-07 08:46:23 +0545

    chore: comment out console.log statement

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ async function checkWeather(city) {
   }
   const data = await response.json();
 
-  console.log(data);
+  // console.log(data);
 
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
